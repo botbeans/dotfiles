b7a1c9b1a3f98d7cc1e35beccfa4f744612ac5b5
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-08-13 08:15:40 -0400

    update: colorschme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,42 +1,44 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      flavour = "frappe",
+      theme = "wave",
       background = {
-        dark = "frappe",
-      },
-      integrations = {
-        blink_cmp = true,
-        gitsigns = true,
-        lsp_trouble = true,
-        mason = true,
-        native_lsp = { enabled = true },
-        noice = true,
-        snacks = true,
-        treesitter = true,
-        which_key = true,
+        dark = "wave",
       },
     },
   },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     flavour = "frappe",
+  --     background = {
+  --       dark = "frappe",
+  --     },
+  --     integrations = {
+  --       blink_cmp = true,
+  --       gitsigns = true,
+  --       lsp_trouble = true,
+  --       mason = true,
+  --       native_lsp = { enabled = true },
+  --       noice = true,
+  --       snacks = true,
+  --       treesitter = true,
+  --       which_key = true,
+  --     },
+  --   },
+  -- },
 
   -- Tell LazyVim to activate it globally on startup
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "kanagawa-wave",
     },
   },
 }
--- return {
---   "folke/tokyonight.nvim",
---   lazy = false,
---   name = "tokyonight",
---   priority = 1000,
---   config = function()
---     vim.cmd([[colorscheme tokyonight]])
---   end,
--- }
